68defb4bde2006c38a8c5b71c8594fdda4bb6be7
James Robertson <james@jamesrobertson.eu>
2018-08-22 14:54:27 +0100

    feature: Added most of the remaining public methods which map to the poublic methods in the drb_fileserver gem.

--- lib/drb_fileserver_plus.rb
+++ lib/drb_fileserver_plus.rb
@@ -2,8 +2,8 @@
 
 # file: drb_fileserver_plus.rb
 
-# description: Designed to provide fault tolerant access to a DRb file 
-#              server when 2 or more back-end nodes are running.
+# description: Designed to provide fault tolerant access to a DRb file server
+#              when 2 or more back-end nodes are running.
 
 require 'drb_fileclient'
 
@@ -14,18 +14,71 @@ class DRbFileServer
     @nodes = nodes.map {|x| 'dfs://' + x}
     @failcount = 0
   end
+  
+  def cp(path, path2)
 
+    file_op do |f|
+      f.cp File.join(@nodes.first, path), File.join(@nodes.first, path2)
+    end
+
+  end    
+
+  def exists?(fname)
+
+    file_op {|f| f.exists? File.join(@nodes.first, fname) }
+
+  end  
+  
+  def ls(path)
+
+    file_op {|f| f.ls File.join(@nodes.first, path) }
+
+  end  
+  
+  def mkdir(path)
+
+    file_op {|f| f.mkdir File.join(@nodes.first, path) }
+
+  end
+  
+  def mkdir_p(path)
+
+    file_op {|f| f.mkdir_p File.join(@nodes.first, path) }
+
+  end
+  
+  def mv(path, path2)
+
+    file_op do |f|
+      f.mv File.join(@nodes.first, path), File.join(@nodes.first, path2)
+    end
+
+  end   
+  
   def read(fname)
 
     file_op {|f| f.read File.join(@nodes.first, fname) }
 
   end
+  
+  def rm(fname)
+
+    file_op {|f| f.rm File.join(@nodes.first, fname) }
+
+  end   
 
   def write(fname, content)
 
     file_op {|f| f.write File.join(@nodes.first, fname), content }
     
   end
+  
+  def zip(fname, a)
+
+    file_op {|f| f.zip File.join(@nodes.first, fname), a }
+    
+  end  
+  
 
   private
 
@@ -64,4 +117,3 @@ class DRbFileServerPlus
   end
 
 end
-
