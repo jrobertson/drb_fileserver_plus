d481132c33260c6c9b9b0fed06a6c158cc8ec502
James Robertson <james@jamesrobertson.eu>
2018-08-23 22:32:04 +0100

    feature: SPS notifications can now be published whenever a file operation takes place.

--- drb_fileserver_plus.gemspec
+++ drb_fileserver_plus.gemspec
@@ -1,11 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'drb_fileserver_plus'
-  s.version = '0.1.1'
+  s.version = '0.2.1'
   s.summary = 'Designed to provide fault tolerant access to a DRb file ' + 
       'server when 2 or more back-end nodes are running.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/drb_fileserver_plus.rb']
-  s.add_runtime_dependency('drb_fileclient', '~> 0.4', '>=0.4.2') 
+  s.add_runtime_dependency('drb_fileclient', '~> 0.4', '>=0.4.4') 
+  s.add_runtime_dependency('sps-pub', '~> 0.5', '>=0.5.5')
   s.signing_key = '../privatekeys/drb_fileserver_plus.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/drb_fileserver_plus.rb
+++ lib/drb_fileserver_plus.rb
@@ -5,20 +5,29 @@
 # description: Designed to provide fault tolerant access to a DRb file server
 #              when 2 or more back-end nodes are running.
 
+require 'sps-pub'
 require 'drb_fileclient'
 
 
 class DRbFileServer
 
-  def initialize(nodes)
+  def initialize(nodes, sps: nil, topic: 'file')
+    
     @nodes = nodes.map {|x| 'dfs://' + x}
     @failcount = 0
+    @sps, @topic = sps, topic
+    
   end
   
   def cp(path, path2)
-
+    
     file_op do |f|
       f.cp File.join(@nodes.first, path), File.join(@nodes.first, path2)
+    end
+    
+    if @sps then
+      @sps.notice "%s/copy: %s %s" % [@topic, File.join(@nodes.first, path), 
+                               File.join(@nodes.first, path2)]
     end
 
   end    
@@ -38,19 +47,32 @@ class DRbFileServer
   def mkdir(path)
 
     file_op {|f| f.mkdir File.join(@nodes.first, path) }
+    
+    if @sps then
+      @sps.notice "%s/mkdir: %s" % [@topic, File.join(@nodes.first, path)]    
+    end
 
   end
   
   def mkdir_p(path)
 
     file_op {|f| f.mkdir_p File.join(@nodes.first, path) }
-
+    
+    if @sps then
+      @sps.notice "%s/mkdir_p: %s" % [@topic, File.join(@nodes.first, path)]
+    end
+    
   end
   
   def mv(path, path2)
 
     file_op do |f|
       f.mv File.join(@nodes.first, path), File.join(@nodes.first, path2)
+    end
+    
+    if @sps then
+      @sps.notice "%s/mv: %s %s" % [@topic, File.join(@nodes.first, path), 
+                               File.join(@nodes.first, path2)]
     end
 
   end   
@@ -64,6 +86,10 @@ class DRbFileServer
   def rm(fname)
 
     file_op {|f| f.rm File.join(@nodes.first, fname) }
+    
+    if @sps then
+      @sps.notice "%s/rm: %s" % [@topic, File.join(@nodes.first, fname)]
+    end
 
   end   
 
@@ -71,11 +97,19 @@ class DRbFileServer
 
     file_op {|f| f.write File.join(@nodes.first, fname), content }
     
+    if @sps then
+      @sps.notice("%s/write: %s" % [@topic, File.join(@nodes.first, fname)])
+    end
+    
   end
   
   def zip(fname, a)
 
     file_op {|f| f.zip File.join(@nodes.first, fname), a }
+    
+    if @sps then
+      @sps.notice "%s/zip: %s" % [@topic, File.join(@nodes.first, fname)]
+    end
     
   end  
   
@@ -103,15 +137,22 @@ end
 class DRbFileServerPlus
 
 
-  def initialize(host: 'localhost', port: '61010', nodes: [])
+  def initialize(host: 'localhost', port: '61010', nodes: [], sps_host: nil, 
+                 sps_port: '59010', sps_topic: 'file')
 
     @host, @port, @nodes = host, port, nodes
+    
+    if sps_host then
+      @sps = SPSPub.new(host: sps_host, port: sps_port) 
+      @topic = sps_topic
+    end
 
   end
 
   def start()
     
-    DRb.start_service "druby://#{@host}:#{@port}", DRbFileServer.new(@nodes)
+    DRb.start_service "druby://#{@host}:#{@port}", 
+        DRbFileServer.new(@nodes, sps: @sps, topic: @topic)
     DRb.thread.join
 
   end
