067381b4b76392aaf76780c4796c86ca6a7f5925
James Robertson <james@jamesrobertson.me.uk>
2022-01-17 21:19:09 +0000

    feature: Added file operation chmod.

--- drb_fileserver_plus.gemspec
+++ drb_fileserver_plus.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'drb_fileserver_plus'
-  s.version = '0.3.1'
+  s.version = '0.3.2'
   s.summary = 'Designed to provide fault tolerant access to a DRb file ' + 
       'server when 2 or more back-end nodes are running.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/drb_fileserver_plus.rb']
-  s.add_runtime_dependency('drb_fileclient', '~> 0.4', '>=0.4.5') 
+  s.add_runtime_dependency('drb_fileclient', '~> 0.6', '>=0.6.0')
   s.add_runtime_dependency('sps-pub', '~> 0.5', '>=0.5.5')
   s.signing_key = '../privatekeys/drb_fileserver_plus.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/drb_fileserver_plus'
 end
--- lib/drb_fileserver_plus.rb
+++ lib/drb_fileserver_plus.rb
@@ -10,160 +10,177 @@ require 'drb_fileclient'
 
 
 class DRbFileServer
-  
+
   attr_accessor :nodes
 
   def initialize(nodes, sps: nil, topic: 'file')
-    
+
     @nodes = nodes
     @failcount = 0
     @sps, @topic = sps, topic
-    
+
   end
-  
-  def cp(path, path2)
-       
+
+  def chmod(permissions, fname)
+
     node = ''
-    
+
     file_op do |f|
-      node = 'dfs://' + @nodes.first      
+      node = 'dfs://' + @nodes.first
+      f.chmod permissions, File.join(node, fname)
+    end
+
+    if @sps then
+      @sps.notice "%s/chmod: %d %s" % [@topic, permissions,
+                                       File.join(node, fname)]
+    end
+
+  end
+
+
+  def cp(path, path2)
+
+    node = ''
+
+    file_op do |f|
+      node = 'dfs://' + @nodes.first
       f.cp File.join(node, path), File.join(node, path2)
     end
-    
+
     if @sps then
-      @sps.notice "%s/copy: %s %s" % [@topic, File.join(node, path), 
+      @sps.notice "%s/copy: %s %s" % [@topic, File.join(node, path),
                                File.join(node, path2)]
     end
 
-  end    
-  
+  end
+
   def directory?(fname)
-        
-    file_op do |f| 
+
+    file_op do |f|
       node = 'dfs://' + @nodes.first
-      f.directory? File.join(node, fname) 
+      f.directory? File.join(node, fname)
     end
 
-  end    
+  end
 
   def exists?(fname)
-        
-    file_op do |f| 
+
+    file_op do |f|
       node = 'dfs://' + @nodes.first
-      f.exists? File.join(node, fname) 
+      f.exists? File.join(node, fname)
     end
 
-  end  
-  
+  end
+
   def ls(path)
-    
-    file_op do |f| 
+
+    file_op do |f|
       node = 'dfs://' + @nodes.first
       f.ls File.join(node, path)
     end
 
-  end  
-  
+  end
+
   def mkdir(path)
 
     node = ''
-    
-    file_op do |f| 
+
+    file_op do |f|
       node = 'dfs://' + @nodes.first
       f.mkdir File.join(node, path)
     end
-    
+
     if @sps then
-      @sps.notice "%s/mkdir: %s" % [@topic, File.join(node, path)]    
+      @sps.notice "%s/mkdir: %s" % [@topic, File.join(node, path)]
     end
 
   end
-  
+
   def mkdir_p(path)
 
-    node = ''    
+    node = ''
 
     file_op do |f|
       node = 'dfs://' + @nodes.first
       f.mkdir_p File.join(node, path)
     end
-    
+
     if @sps then
       @sps.notice "%s/mkdir_p: %s" % [@topic, File.join(node, path)]
     end
-    
+
   end
-  
+
   def mv(path, path2)
 
     node = ''
-    
+
     file_op do |f|
       node = 'dfs://' + @nodes.first
       f.mv File.join(node, path), File.join(node, path2)
     end
-    
+
     if @sps then
-      @sps.notice "%s/mv: %s %s" % [@topic, File.join(node, path), 
+      @sps.notice "%s/mv: %s %s" % [@topic, File.join(node, path),
                                File.join(node, path2)]
     end
 
-  end   
-  
+  end
+
   def read(fname)
-    
+
     file_op do |f|
       node = 'dfs://' + @nodes.first
       f.read File.join(node, fname)
     end
 
   end
-  
+
   def rm(fname)
 
     node = ''
-    
+
     file_op do |f|
       node = 'dfs://' + @nodes.first
       f.rm File.join(node, fname)
     end
-    
+
     if @sps then
       @sps.notice "%s/rm: %s" % [@topic, File.join(node, fname)]
     end
 
-  end   
+  end
 
   def write(fname, content)
 
     node = ''
-    
+
     file_op do |f|
       node = 'dfs://' + @nodes.first
       f.write File.join(node, fname), content
     end
-    
+
     if @sps then
       @sps.notice("%s/write: %s" % [@topic, File.join(node, fname)])
     end
-    
+
   end
-  
+
   def zip(fname, a)
-    
+
     node = ''
-    
+
     file_op do |f|
       node = 'dfs://' + @nodes.first
       f.zip File.join(node, fname), a
     end
-    
+
     if @sps then
       @sps.notice "%s/zip: %s" % [@topic, File.join(node, fname)]
     end
-    
-  end  
-  
+
+  end
+
 
   private
 
@@ -189,21 +206,21 @@ end
 class DRbFileServerPlus
 
 
-  def initialize(host: 'localhost', port: '61010', nodes: [], sps_host: nil, 
+  def initialize(host: 'localhost', port: '61010', nodes: [], sps_host: nil,
                  sps_port: '59010', sps_topic: 'file')
 
     @host, @port, @nodes = host, port, nodes
-    
+
     if sps_host then
-      @sps = SPSPub.new(host: sps_host, port: sps_port) 
+      @sps = SPSPub.new(host: sps_host, port: sps_port)
       @topic = sps_topic
     end
 
   end
 
   def start()
-    
-    DRb.start_service "druby://#{@host}:#{@port}", 
+
+    DRb.start_service "druby://#{@host}:#{@port}",
         DRbFileServer.new(@nodes, sps: @sps, topic: @topic)
     DRb.thread.join
 
